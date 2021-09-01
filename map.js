b299a8c0a393c7681abc63cc87d7abcd98cfc33e
taralyh <89719122+taralyh@users.noreply.github.com>
2021-08-31 21:32:07 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,4 +1,31 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([48.5, -122.5], 13);
+
+var marker = L.marker([48.5, -122.5]).addTo(mymap);
+
+var circle = L.circle([48.505, -122.505], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+
+var polygon = L.polygon([
+    [48.509, -122.58],
+    [48.503, -122.56],
+    [48.51, -122.57]
+]).addTo(mymap);
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(boatpoints.geoson, {
+    style: myStyle
+}).addTo(map);
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
