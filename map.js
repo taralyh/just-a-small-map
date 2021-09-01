491a61f55fdf127f958414de036b465fe71c7a73
taralyh <89719122+taralyh@users.noreply.github.com>
2021-09-01 08:11:21 -0700

    Update map.js

--- map.js
+++ map.js
@@ -19,11 +19,6 @@ var polygon = L.polygon([
 L.geoJSON(boatpoints.geojson).addTo(map);
 
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/2/0/1?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZGlzZXF1aWxpYnJpdW0iLCJhIjoiY2t0MHplNWxlMDF2bzJvcGNjNDQ4c2VwYiJ9.C-U-EoZNVVsdr84Nai8zBw'
-}).addTo(mymap);
+L.tileLayer.provider(Esri.WorldImagery, {
+      maxZoom: 18
+}).addTo(map);
