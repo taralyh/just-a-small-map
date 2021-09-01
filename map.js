d6a0a9694de8d5d09c83817537f1956c5c03e671
taralyh <89719122+taralyh@users.noreply.github.com>
2021-09-01 08:51:41 -0700

    Update map.js

--- map.js
+++ map.js
@@ -19,9 +19,18 @@ var polygon = L.polygon([
 L.geoJSON(boatpoints.geojson).addTo(mymap);
 
 
-L.tileLayer.provider(Esri.WorldImagery, {
+/*L.tileLayer.provider(Esri.WorldImagery, {
       maxZoom: 18
-}).addTo(mymap);
+}).addTo(mymap);*/
+
+mapLink = '<a href="http://www.esri.com/">Esri</a>';
+wholink = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+
+L.tileLayer(
+            'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            attribution: '&copy; '+mapLink+', '+wholink,
+            maxZoom: 18,
+            }).addTo(mymap);
 
 /*L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/2/0/1?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
