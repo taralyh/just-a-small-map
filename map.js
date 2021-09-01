27c746f575d23e51ece4a0cda1a730b44201cefa
taralyh <89719122+taralyh@users.noreply.github.com>
2021-08-31 21:43:01 -0700

    Update map.js

--- map.js
+++ map.js
@@ -19,7 +19,7 @@ var polygon = L.polygon([
 L.geoJSON(boatpoints.geojson).addTo(map);
 
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/2/0/1?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
