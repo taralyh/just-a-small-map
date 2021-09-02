49f1c328a909220ed05f7317255ea7d37f442638
taralyh <89719122+taralyh@users.noreply.github.com>
2021-09-01 17:01:56 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,27 +1,4 @@
-var mymap = L.map('mapid').setView([48.5, -122.5], 13);
-
-/*var marker = L.marker([48.5, -122.5]).addTo(mymap);
-
-var circle = L.circle([48.505, -122.505], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-
-
-var polygon = L.polygon([
-    [48.509, -122.58],
-    [48.503, -122.56],
-    [48.51, -122.57]
-]).addTo(mymap);*/
-
-L.tileLayer.provider('Stamen.Watercolor').addTo(mymap);
-
-var geojsonLayer = new L.GeoJSON.AJAX("boatpoints.geojson");
-geojsonLayer.addto(mymap);
-
-$.ajax({
+var boats = $.ajax({
  url:'boatpoints.geojson',
  dataType: "json",
  success: console.log("Data successfully loaded!"),
@@ -29,3 +6,30 @@ $.ajax({
     alert(xhr.statusText)
  }
 })
+
+// Load data with .ajax()
+$.when(boats).done(function(){
+
+    var mymap = L.map('mapid').setView([48.5, -122.5], 13);
+
+    /*var marker = L.marker([48.5, -122.5]).addTo(mymap);
+
+    var circle = L.circle([48.505, -122.505], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(mymap);
+
+
+    var polygon = L.polygon([
+        [48.509, -122.58],
+        [48.503, -122.56],
+        [48.51, -122.57]
+    ]).addTo(mymap);*/
+
+    L.tileLayer.provider('Stamen.Watercolor').addTo(mymap);
+
+    L.geoJSON(boats.responseJSON).addTo(mymap);
+
+});
